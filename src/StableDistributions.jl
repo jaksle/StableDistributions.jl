6f4e229bfc41d0f0d204641da5d127c3cf7cd8b9
Jakub Ślęzak <128084860+jaksle@users.noreply.github.com>
2024-03-13 00:57:43 +0100

    Update StableDistributions.jl

--- src/StableDistributions.jl
+++ src/StableDistributions.jl
@@ -9,15 +9,14 @@ import Distributions: @check_args, @distr_support, @quantile_newton,
     params, shape, location, scale, minimum, maximum,
     convert, convolve, +, *, partype, mean, var, skewness, kurtosis,
     quantile_newton, cquantile_newton, invlogcdf_newton, invlogccdf_newton,
-    mgf, cf, pdf, logpdf, cdf, fit
+    mgf, cf, pdf, logpdf, cdf, quantile, fit
 
 
 export Stable,
     rand,
     params, shape, location, scale, minimum, maximum,
     convert, convolve, +, *, partype, mean, var, skewness, kurtosis,
-    quantile_newton, cquantile_newton, invlogcdf_newton, invlogccdf_newton,
-    mgf, cf, pdf, logpdf, cdf, mgf, fit, fit_quantile
+    mgf, cf, pdf, logpdf, cdf, mgf, quantile, fit, fit_quantile
 
 include("stable.jl")
 include("conversion.jl")
