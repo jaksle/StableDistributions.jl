b481601a4d1c7d5a3c6187a246ac1c47be3f751d
Jakub Ślęzak <128084860+jaksle@users.noreply.github.com>
2024-03-13 00:47:56 +0100

    Update stable.jl

--- src/stable.jl
+++ src/stable.jl
@@ -172,6 +172,8 @@ function cdf(d::Stable{T}, x::Real) where T
     end
 end
 
+quantile(d::Stable, p::Real) = Distributions.quantile_newton(d,p)
+
 @quantile_newton Stable
 
 #### Affine transformations
