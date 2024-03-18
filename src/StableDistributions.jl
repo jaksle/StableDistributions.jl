a0e3b44ff8930e9495d346c487ec1aa66f2f0a39
Jakub Ślęzak <128084860+jaksle@users.noreply.github.com>
2024-03-18 17:46:51 +0100

    adding temporarly Interpolations dependence

--- src/StableDistributions.jl
+++ src/StableDistributions.jl
@@ -6,6 +6,7 @@ using Distributions
 using SpecialFunctions
 using QuadGK: quadgk
 using StatsFuns: logexpm1, log1mexp
+import Interpolations: interpolate, Gridded, Linear
 import Distributions: @check_args, @distr_support,
     params, shape, location, scale, support, minimum, maximum,
     convert, convolve, +, *,
