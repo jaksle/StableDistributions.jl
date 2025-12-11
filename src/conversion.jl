56d72ae5f0e5f9d6d2b34f216776809b55d96277
Jakub Ślęzak <128084860+jaksle@users.noreply.github.com>
2025-12-11 14:04:06 +0100

    Update conversion.jl

--- src/conversion.jl
+++ src/conversion.jl
@@ -1,6 +1,5 @@
 
-
-
 convert(::Type{Stable}, d::Normal) = Stable(2, 0, d.σ/√2, d.μ)
 convert(::Type{Stable}, d::Cauchy) = Stable(1, 0, d.σ, d.μ)
 convert(::Type{Stable}, d::Levy) = Stable(1/2, 1, d.σ, d.μ)
+
